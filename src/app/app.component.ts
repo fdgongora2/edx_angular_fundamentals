cd0a9fc026e3454b5942279e716da9e1ee78f224
David Góngora <fdgongora@iesmurgi.org>
2020-04-02 06:38:33 +0200

    Servicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GitSearchService } from './git-search.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponentimplements implements OnInit {
+
+  private GitSearchServiceLocal: GitSearchService; 
+  constructor(GitSearchService2: GitSearchService) {
+    this.GitSearchServiceLocal = GitSearchService2;
+
+  }
+
+  ngOnInit() {
+    this.GitSearchServiceLocal.gitSearch('angular').then( (response) => {
+      alert("Total Libraries Found:" + response.total_count);
+    }, (error) => {
+      alert("Error: " + error.statusText)
+    })
+  }
   title = 'GitHub Browser';
 }
