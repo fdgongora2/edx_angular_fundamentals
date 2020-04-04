ba47ee5dc0227b0f113651676a4de72a359832c0
David Góngora <fdgongora@iesmurgi.org>
2020-04-04 16:35:06 +0200

    Advanced - To Validate a Reactive Form

--- src/app/git-search/git-search.component.ts
+++ src/app/git-search/git-search.component.ts
@@ -3,7 +3,7 @@ import { GitSearchService } from '../git-search.service'
 import { GitSearch } from '../git-search'
 import { ActivatedRoute, ParamMap, Router } from '@angular/router'
 import { AdvancedSearchModel } from '../advanced-search-model'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-git-search',
@@ -22,15 +22,33 @@ export class GitSearchComponent implements OnInit {
   constructor(private GitSearchService: GitSearchService, private route: ActivatedRoute, private router: Router) {
 
     this.modelKeys.forEach((key) => {
-      this.formControls[key] = new FormControl();
+      let validators = [];
+      if (key === 'q') {
+        validators.push(Validators.required);
+      }
+      if (key === 'stars') {
+        validators.push(Validators.maxLength(4))
+      }
+      validators.push(this.noSpecialChars);
+      this.formControls[key] = new FormControl(this.model[key], validators);
     })
-    
+
     this.form = new FormGroup(this.formControls);
   }
 
   model = new AdvancedSearchModel('', '', '', null, null, '');
   // Creamos un array con los atributos del objeto 'model' para procesarlo con *ngFor
   modelKeys = Object.keys(this.model);
+
+  noSpecialChars(c: FormControl) {
+    let REGEXP = new RegExp(/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/);
+
+    return REGEXP.test(c.value) ? {
+      validateEmail: {
+        valid: false
+      }
+    } : null;
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
@@ -61,19 +79,19 @@ export class GitSearchComponent implements OnInit {
     // this.searchResults = null;  
     this.pagina = 1;
     this.searchResults = null;
-    let search : string = this.form.value['q'];
-    let params : string = "";
-    this.modelKeys.forEach(  (elem) => {
-        if (elem === 'q') {
-            return false;
-        }
-        if (this.form.value[elem]) {
-            params += '+' + elem + ':' + this.form.value[elem];
-        }
+    let search: string = this.form.value['q'];
+    let params: string = "";
+    this.modelKeys.forEach((elem) => {
+      if (elem === 'q') {
+        return false;
+      }
+      if (this.form.value[elem]) {
+        params += '+' + elem + ':' + this.form.value[elem];
+      }
     })
     this.searchQuery = search;
     if (params !== '') {
-        this.searchQuery = search + params;
+      this.searchQuery = search + params;
     }
     this.displayQuery = this.searchQuery;
     this.gitSearch();
